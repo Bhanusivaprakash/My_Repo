80baaac72083878f4e9584530a967157c4335690
Bhanusivaprakash <46567768+Bhanusivaprakash@users.noreply.github.com>
2026-08-09 07:28:32 +0530

    Refactor coprocessor with async signals and cleanup
    Refactored coprocessor design to include asynchronous control signals and removed unused DIV and SQUARE tasks. Added synchronization flip-flops for write, read, address, and execute signals.

--- Coprocessor Design/coprocessor_design_V2.v
+++ Coprocessor Design/coprocessor_design_V2.v
@@ -1,22 +1,24 @@
 module coprocessor(
 
     input wire clk,
-    input wire signed [15:0] data_in,
-    output reg signed [15:0] data_out,
+    inout wire [15:0] data,
     output reg error,
-    input wire write_enable,
-    input wire read_enable,
+    input wire write_enable_async,
+    input wire read_enable_async,
     input wire rst,
     input wire [4:0] operation,
-    input wire address_enable,
+    input wire address_enable_async,
     input wire [4:0] address,
-    input wire execute,
-    output reg done
+    input wire execute_async,
+    output reg done,
+    output reg ack
 
 );
 
 reg signed [31:0] memory [0:31];
-reg signed [31:0] op_memory [0:31];
+
+reg signed [15:0] data_in;
+reg signed [15:0] data_out;
     
 reg signed [31:0] acc = 0;
 reg signed [31:0] result = 0;
@@ -80,46 +82,16 @@ endtask
 task MAC;
 begin
     if(execution_ptr < write_ptr) begin
-        temp_res = acc + ((memory[2*execution_ptr] * memory[2*execution_ptr + 1]) >>> 16);
+        if(2*execution_ptr+1 < write_ptr) begin
+            temp_res = acc + ((memory[2*execution_ptr] * memory[2*execution_ptr + 1]) >>> 16);
+        end
+        else begin
+            temp_res = acc + (memory[2*execution_ptr]);
+        end
         acc <= temp_res;
         result <= temp_res;    
         execution_ptr <= execution_ptr + 1;
     end
-end
-endtask
-
-task DIV;
-begin
-
-    if (execution_ptr == 0) begin
-        prod_acc = memory[0];
-        
-        execution_ptr <= 1;
-    end
-
-    else if (execution_ptr < write_ptr) begin
-
-        if (memory[execution_ptr] == 0) begin
-            error <= 1;
-        end
-        else begin
-            temp_res = (prod_acc<<<16)/memory[execution_ptr];
-            $display("TEMP=%h", prod_acc);
-            prod_acc <= temp_res;
-            acc <= temp_res;
-            sum_acc <= temp_res;
-            result <= temp_res;
-            execution_ptr <= execution_ptr + 1;
-            error <= 0;
-        end
-    end
-end
-endtask
-
-task SQUARE;
-begin
-    temp_res = (memory[address] * memory[address]);
-    result <= temp_res >>> 16;
 end
 endtask
 
@@ -147,14 +119,46 @@ endtask
 
 task CLEAR;
 begin
-  //  op_memory[address] <= 0;
     sum_acc <= 0;
     prod_acc <= 32'h00010000; // NOT 1;
 end
 endtask
 
+reg write_ff1, write_ff2;   // Sync flipflops
 always @(posedge clk) begin
+    write_ff1 <= write_enable_async;
+    write_ff2 <= write_ff1;
+end
 
+wire write_enable = write_ff2;
+
+reg read_ff1, read_ff2;   // Sync flipflops
+always @(posedge clk) begin
+    read_ff1 <= read_enable_async;
+    read_ff2 <= read_ff1;
+end
+
+wire read_enable = read_ff2;
+assign data = read_enable ? data_out : 16'bz;
+
+reg address_ff1, address_ff2;   // Sync flipflops
+always @(posedge clk) begin
+    address_ff1 <= address_enable_async;
+    address_ff2 <= address_ff1;
+end
+
+wire address_enable = address_ff2;
+
+reg exec_ff1, exec_ff2;   // Sync flipflops
+always @(posedge clk) begin
+    exec_ff1 <= execute_async;
+    exec_ff2 <= exec_ff1;
+end
+
+wire execute = exec_ff2;
+
+always @(posedge clk) begin
+    
     if(rst) begin
         write_ptr <= 0;
         execution_ptr <= 0;
@@ -164,10 +168,14 @@ always @(posedge clk) begin
         prod_acc <= 32'h00010000; // NOT 1;
         done <= 0;
         word_select <= 0;
+        write_lock <= 0;
+        read_lock <= 0;
+        error <= 0;
+        result <= 0;;
+        ack <= 0;
         
         for(i = 0; i < 32; i = i + 1) begin
-            memory[i] <= 16'h00;
-            op_memory[i] <= 16'h00;
+            memory[i] <= 32'h00;
         end
     end
 
@@ -180,24 +188,25 @@ always @(posedge clk) begin
         if(write_enable && !write_lock) begin
             $display("WRITE: ptr=%0d data=%0d",
              write_ptr,
-             data_in);
+             data);
             if(word_select == 0) begin
-                memory[write_ptr][15:0] <= data_in;
-                op_memory[write_ptr][15:0] <= data_in;
+                memory[write_ptr][15:0] <= data;
                 write_lock        <= 1;
+                ack <= 1;
                 word_select <= 1;
             end
             else if(word_select == 1) begin
-                memory[write_ptr][31:16] <= data_in;
-                op_memory[write_ptr][31:16] <= data_in;
+                memory[write_ptr][31:16] <= data;
                 write_ptr         <= write_ptr + 1;
                 write_lock        <= 1;
+                ack <= 1;
                 word_select <= 0;
             end
             
         end
         else if(!write_enable) begin
             write_lock <= 0;
+            ack <= 0;
         end
 
         if(!execute) begin
@@ -225,17 +234,6 @@ always @(posedge clk) begin
                 4'b0011:    begin   //PRODUCT;
                     MUL;
                     done <= (write_ptr != 0) && (execution_ptr == write_ptr - 1);
-                end
-
-                4'b0100:    begin   //DIVISION;
-                    DIV;
-                    done <= (write_ptr != 0) && (execution_ptr == write_ptr - 1);
-                end
-
-                4'b0101:    begin   //SQUARE;
-                    SQUARE;
-                    //done <= (write_ptr != 0) && (scalar_ptr == write_ptr - 1);
-                    done <= 1;
                 end
 
                 4'b0110:    begin   //ABS;
@@ -275,20 +273,39 @@ always @(posedge clk) begin
                 data_out <= result[15:0];
                 //read_ptr <= read_ptr + 1;
                 read_lock <= 1; 
+                ack <= 1;
                 word_select <= 1;
             end
             else if (word_select == 1) begin
                 data_out <= result[31:16];
                 read_lock <= 1;
+                ack <= 1;
                 word_select <= 0;
             end
             
         end
         else if(!read_enable) begin
             read_lock <= 0;
+            ack <= 0;
         end
     end
     
 end
 
+ila_0 ila_dbg (
+	.clk(clk), // input wire clk
+	.probe0(result), // input wire [31:0] probe0
+	.probe1(memory[0]),
+	.probe2(operation),
+	.probe3(address),
+	.probe4(rst),
+	.probe5(write_enable),
+	.probe6(read_enable),
+	.probe7(address_enable),
+	.probe8(execute),
+	.probe9(done),
+	.probe10(ack)
+);
+
 endmodule
+
